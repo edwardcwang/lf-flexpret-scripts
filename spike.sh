b6cb26142145a04a37242cc2a502ba4f01294225
Edward Wang <edward.c.wang@compdigitec.com>
2020-02-25 17:42:35 -0500

    Print commit log

--- spike.sh
+++ spike.sh
@@ -6,4 +6,5 @@ set -euo pipefail
 pc_start=$(riscv64-unknown-elf-objdump -x Minimal | grep "0 _start" | cut -d " " -f1)
 echo "PC starts at: ${pc_start}"
 
-spike --isa=RV32IMAC -m0x20000000:0x2000,0x40000000:0x1000,0x80000000:0x20000 -d $1
+# Use -d for single-step debugging
+spike --isa=RV32IMAC -m0x20000000:0x2000,0x40000000:0x1000,0x80000000:0x20000 -l $1
