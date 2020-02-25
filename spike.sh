420c21133f739a966b68f906074a1aaa85162979
Edward Wang <edward.c.wang@compdigitec.com>
2020-02-25 14:57:17 -0500

    Let spike pull the _start address from the ELF file

--- spike.sh
+++ spike.sh
@@ -2,4 +2,8 @@
 set -ex
 set -euo pipefail
 
-spike --isa=RV32IMAC -m0x20000000:0x2000,0x40000000:0x1000,0x80000000:0x20000 --pc=0x80000100 -d $1
+# Grab the PC start address from the ELF file
+pc_start=$(riscv64-unknown-elf-objdump -x Minimal | grep "0 _start" | cut -d " " -f1)
+echo "PC starts at: ${pc_start}"
+
+spike --isa=RV32IMAC -m0x20000000:0x2000,0x40000000:0x1000,0x80000000:0x20000 -d $1
